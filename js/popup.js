f3650a28a033dcdf05e2beade038d0560bdd8060
dbomard <david.bomard@laposte.net>
2022-09-02 23:30:40 +0200

    css changed

--- js/popup.js
+++ js/popup.js
@@ -1,1 +1,16 @@
 /** script.js DBO20220816 **/
+
+function show_popup(param) {
+    let $popup_dialog = document.querySelector("dialog");
+
+    $popup_dialog.showModal();
+}
+
+function close_dialog() {
+    document.querySelector("audio").pause();
+    document.querySelector("dialog").close();
+}
+
+function load_xml_file($path) {
+    let $file = new FileReader();
+}
